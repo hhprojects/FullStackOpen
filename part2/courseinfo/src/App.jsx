e676023a3a84e906921fb3a023b93bcd4c312904
Han Hua <lhanhua01@gmail.com>
2024-06-29 01:33:10 +0800

    Completed 2.1 - 2.5

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,82 @@
+import { useState } from 'react'
+import Course from './components/Course'
+
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((accumulator, {exercises}) => accumulator + exercises, 0 )
+  console.log("Total: " , total)
+  return <p><b>total of {total} exercises</b></p>
+}
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => {
+
+  return (
+      parts.map((part) => {
+        console.log("Part:", part);
+        return (
+          <Part
+            key={part.id}
+            part={part} 
+          />
+        )
+      })      
+  )
+}
+
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return courses.map(course => <Course course={course} />)
+}
+
+export default App
---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,11 @@
+
+const Course = ({course}) =>{
+    console.log
+    return (
+      <div>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+      </div>
+    )
+  }
