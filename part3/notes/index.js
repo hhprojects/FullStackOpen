77134db67d603bcb8e1208c6d4ef71ee61ea201f
Han Hua <lhanhua01@gmail.com>
2024-12-27 18:26:08 +0800

    Completed POST and GET for Notes using mongoose

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const Note = require('./models/note')
@@ -33,13 +34,9 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) =>{
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  if (note){
+  Note.findById(request.params.id).then(note =>{
     response.json(note)
-  }else{
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -49,31 +46,21 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
-
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({error: 'content missing'})
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: Boolean(body.important) || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 
